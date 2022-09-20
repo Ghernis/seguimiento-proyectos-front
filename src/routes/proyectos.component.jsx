b1588fe804973e2ab2aacb4fb089b5f4f1e01595
Hernan Gomez <hernanegomez@gmail.com>
2022-09-20 00:09:58 -0300

    pagina proyectos y hashrouter

--- src/routes/proyectos.component.jsx
+++ src/routes/proyectos.component.jsx
@@ -1,6 +1,38 @@
+import Tabla from '../components/tabla/tabla.component';
+import { useState, useEffect } from 'react';
+
 const Proyectos = ()=>{
+    const [proyectos, setProyectos] = useState([]);
+    const proyectosHeader = [
+        {
+            key:'id',
+            titulo:'id'
+        },
+        {
+            key:'titulo',
+            titulo:'Titulo'
+        },
+        {
+            key:'descripcion',
+            titulo:'Descripcion'
+        },
+    ]
+    useEffect(()=>{
+        fetch('https://codemia-proyectos-production.up.railway.app/proyectos')
+            .then(res=> res.json())
+            .then(proy=>setProyectos(proy))
+    },[]);
     return (
-    <div>proyetos</div>
+        <div className={'container'}>
+
+            <section className='section'>
+            <h1 className='title'>Template Proyectos</h1>
+            <br />
+            <section className='section'>
+            <Tabla data={proyectos} headers={proyectosHeader} />
+            </section>
+            </section>
+        </div>
     )
 }
 
